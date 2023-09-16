1c7b3471090726c098787ee0865829f8694a71fa
Elton Schivei Costa <costa@elton.schivei.nom.br>
2023-09-16 00:27:18 -0300

    Secrets

--- tasks.js
+++ tasks.js
@@ -7,6 +7,14 @@ import url from 'url'
 const __dirname = url.fileURLToPath(new URL('.', import.meta.url))
 const serviceAccount = JSON.parse(await fs.readFile(join(__dirname, "firebase-admin.json"), 'utf8'));
 
+if (!serviceAccount.private_key_id) {
+    serviceAccount.private_key_id = process.env.FIREBASE_PRIVATE_KEY_ID;
+
+    if (!serviceAccount.private_key_id) {
+        throw new Error('Missing private key id')
+    }
+}
+
 if (!serviceAccount.private_key) {
     serviceAccount.private_key = process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n');
 
